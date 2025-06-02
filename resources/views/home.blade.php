93d299b071cc4c16c1eb1aa7c7c0248fb4ea4707
Francisco Campos <camposfrancisco2002@gmail.com>
2025-06-02 04:21:09 +0100

    Update navigation icons and welcome message: replace user icon with home icon in navigation, and change greeting from "Bem-vindo" to "Olá"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
     <div class="nav-bar d-flex justify-content-between align-items-center mb-4 p-3">
         @if (Auth::check())
             <a href="{{ route('home') }}" class="decoration-none text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+                    <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
                 </svg>
             </a>
             <div class="d-flex">
@@ -73,7 +73,7 @@
         <div class="row">
             @if(Auth::check())
                 <div class="col-12 mb-4">
-                    <h4>Bem-vindo, {{ Auth::user()->name }}!</h4>
+                    <h4>Olá, {{ Auth::user()->name }}!</h4>
                 </div>
             @endif
             <h1 class="mb-3">Categorias</h1>
